a4a863b8205acb595940418a2679f6b4ce012c3e
pablotardio <pablo.tv30@gmail.com>
2021-01-07 16:41:27 -0400

    terminado el login

--- lib/src/providers/usuario_provider.dart
+++ lib/src/providers/usuario_provider.dart
@@ -3,7 +3,10 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class UsuarioProvider {
-  final url = '';
+  final _url = 'localhost:3002';
+  
+
+
   Future<Map<String,dynamic>> nuevoUsuario(String email, String password) async {
     final authdata = {
       'email': email,
@@ -11,7 +14,7 @@ class UsuarioProvider {
       'rolId': 3,
       'returnSecureToken': true
     };
-    final response = await http.post(url, body: json.encode(authdata));
+    final response = await http.post(_url, body: json.encode(authdata));
     Map<String, dynamic> decodedResp = json.decode(response.body);
     print(decodedResp);
     if (decodedResp.containsKey('token')) {
@@ -25,5 +28,39 @@ class UsuarioProvider {
         'ok':false
       };
     }
+  }
+
+  Future<Map<String,dynamic>> login(String email, String password) async {
+   try {
+      final authdata = {
+      'email': email,
+      'password': password,
+      'rolId': 3,
+      'returnSecureToken': true
+    };
+    final headers= {
+        "Content-Type": "application/json"
+      };
+    
+    final response = await http.post(new Uri.http('192.168.1.4:3002', '/api/login'),
+    headers:headers,
+    body:json.encode(authdata),
+     );
+    Map<String, dynamic> decodedResp = json.decode(response.body);
+   // print(decodedResp);
+    if (decodedResp.containsKey('token')) {
+      return {
+        'ok': true,
+        'token': decodedResp['token'],
+        'vistas': decodedResp['vistas']
+      };
+    }else{
+      return {
+        'ok':false
+      };
+    }
+   } catch (e) {
+     print(e);
+   }
   }
 }
