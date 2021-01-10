064c42a2307aebd49d275152cb49688444cd447a
pablotardio <pablo.tv30@gmail.com>
2021-01-10 17:06:19 -0400

    cambiado jpg por png

--- lib/src/views/widgets/card_swiper_widget.dart
+++ lib/src/views/widgets/card_swiper_widget.dart
@@ -20,7 +20,7 @@ class CardSwiper extends StatelessWidget {
           return ClipRRect(
             borderRadius: BorderRadius.circular(20.0),
             child: FadeInImage(
-              placeholder: AssetImage('assets/img/no-image.jpg'),
+              placeholder: AssetImage('assets/img/no-image.png'),
               image: NetworkImage(fotosDePerfil[index]['url']),
               fit: BoxFit.cover,
             ),
