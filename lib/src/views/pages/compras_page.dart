05acf7491bfb56f8e0292fe953b6dc0ff9d49830
pablotardio <pablo.tv30@gmail.com>
2021-01-11 16:36:12 -0400

    compras rediseñado a un listview

--- lib/src/views/pages/compras_page.dart
+++ lib/src/views/pages/compras_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class ComprasPage extends StatefulWidget {
   ComprasPage({Key key}) : super(key: key);
 
@@ -9,69 +10,39 @@ class ComprasPage extends StatefulWidget {
 class _ComprasPageState extends State<ComprasPage> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
         appBar: AppBar(
-          title: Text('Escanear Evento'),
+          title: Text('Compras'),
         ),
-        body:Container(
+        body: Container(
             padding: EdgeInsets.all(30.0),
-            child: Center(
-                child: Column(children: [
-           
-              SizedBox(
-                height: 20.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  _botonCarrito(),
-                  SizedBox(
-                    width: 20.0,
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              _botonGuardar(),
-            ]
-            )
-            )
-            )
-            );
-  
-  }
-  _botonCarrito() {
-    return (FloatingActionButton.extended(
-      label:Text('Hola'),
-      icon: Icon(Icons.qr_code) ,
-      onPressed: _escanearQR,
-    ));
+            child: Center(child: ListView(children: [_menuItemCarrito(),SizedBox(height: 25.0,),_menuItemComprados()]))));
   }
 
-
-  _escanearQR() async {
-    
+  _menuItemCarrito() {
+    return ListTile(
+      leading: Icon(Icons.shopping_cart),
+      title: Text('Mi carrito'),
+      subtitle: Text('Aqui puede ver todas sus fotos y pagarlas'),
+      trailing: Icon(Icons.arrow_forward_ios),
+      onTap: () {},
+    );
   }
 
-  _botonGuardar() {
-    return (RaisedButton.icon(
-      icon: Icon(Icons.save),
-      label: Text('Guardar'),
-      textColor: Colors.white,
-      color: Colors.orange[900],
-      elevation: 5,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-      onPressed: () {
-        
-      },
-    ));
-  }
+  _escanearQR() async {}
 
+  _menuItemComprados() {
+    return ListTile(
+      leading: Icon(Icons.photo_album),
+      title: Text('Fotos Compradas'),
+      subtitle: Text('Ver y descargar todas sus fotos compradas'),
+      trailing: Icon(Icons.arrow_forward_ios),
+      onTap: () {},
+    );
+  }
 
   @override
   void dispose() {
     super.dispose();
-    
   }
 }
